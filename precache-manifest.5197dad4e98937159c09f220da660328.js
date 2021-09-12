d920d8d4caa0fb0b9cdf99a9fa183ef0a1d74a16
twoGophers <falinvlad93@rambler.ru>
2021-09-12 18:30:42 +0300

    deploy

---
+++ precache-manifest.5197dad4e98937159c09f220da660328.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf2a7a0ed5e186c8bfe",
+    "url": "/parallax_vue/css/app.783aeac6.css"
+  },
+  {
+    "revision": "d26fc6ecfbd701a374ed523abba7b100",
+    "url": "/parallax_vue/img/0.d26fc6ec.png"
+  },
+  {
+    "revision": "673cd4b9e760698065b779fff02aa1eb",
+    "url": "/parallax_vue/img/1.673cd4b9.png"
+  },
+  {
+    "revision": "0a8a3db5f6d2c68af44183e47ba5fc2f",
+    "url": "/parallax_vue/img/dust.0a8a3db5.png"
+  },
+  {
+    "revision": "e009339085bdaeda8c11cd62d32063b4",
+    "url": "/parallax_vue/index.html"
+  },
+  {
+    "revision": "4e70482a6a7fb194cbb4",
+    "url": "/parallax_vue/js/about.7a8ca7f9.js"
+  },
+  {
+    "revision": "daf2a7a0ed5e186c8bfe",
+    "url": "/parallax_vue/js/app.b4b422d8.js"
+  },
+  {
+    "revision": "3ef8a3f3c16c92ce10b9",
+    "url": "/parallax_vue/js/chunk-vendors.bb2d815b.js"
+  },
+  {
+    "revision": "ef1f754de0f064554046f763528bd5d8",
+    "url": "/parallax_vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/parallax_vue/robots.txt"
+  }
+]);
